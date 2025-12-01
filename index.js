9a724f5af58e8320355ff1f3343e32669bba8000
Salim Dewani <10102048+ssdewani@users.noreply.github.com>
2025-12-01 23:04:15 +0000

    cors 4

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const client = new openai({
 
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://small-fe.vercel.app/'],
+  origin: ['http://localhost:5173', 'https://small-fe.vercel.app'],
   methods: 'GET,POST,PUT,DELETE,PATCH,HEAD',
   credentials: true,
 }));
